ee25a0368c850233731eae48714dfecc20ff2956
syimyuzya <syimyuzya@gmail.com>
2020-12-15 13:39:11 +0800

    Update README.md

--- demo/build_demo.sh
+++ demo/build_demo.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-dot -Tpng char.dot -o char.png
-dot -Tpng route.dot -o route.png
-dot -Kfdp -Tpng example1.dot -o example1.png
-dot -Kfdp -Tpng example2.dot -o example2.png
+#! /usr/bin/env bash
+
+for i in *.gv; do
+    dot -Tpng "$i" -o "${i%.gv}.png"
+done
